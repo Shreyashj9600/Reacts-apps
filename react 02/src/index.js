b10f4d6bc35fde7d9d4f8dac29ed501abdc476bd
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-06-19 02:07:28 +0530

    react.js

---
+++ react 02/src/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+// import React from 'react';
+// import ReactDom from 'react-dom';
+
+
+ReactDom.render(
+  [
+    <h1>hello world !</h1>,
+    <p>this is a paragraph</p>,
+  ],
+  document.getElementById('root')
+
+);
