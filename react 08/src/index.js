bfebc263161648ee70b15a63817b60b67a757c36
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-06-20 16:09:42 +0530

    JSX Attributes in ReactJS

---
+++ react 08/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+ReactDom.render(
+  <>
+    <h1 contentEditable="true">Hello, My name is shreyash</h1>
+    <a href='https://webecom.netlify.app/' target='_shreyash'>  
+    <img src='https://images.unsplash.com/photo-1426604966848-d7adac402bff?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ' alt='nature-img' />
+    </a>
+  </>,
+  document.getElementById("root")
+);
