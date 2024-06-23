27204a63ec5ddc167e646eff226672e75a656e75
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-06-24 02:41:57 +0530

    Netflix web

---
+++ react 15/src/Sdata.jsx
@@ -0,0 +1,22 @@
+const Sdata = [
+    {
+        imgsrc: 'https://wallpapercave.com/wp/wp4056410.jpg',
+        title: 'A Netflix Orignal Serise',
+        sname: 'Dark',
+        link: 'https://www.netflix.com/in/title/80990668?source=35'
+    },
+    {
+        imgsrc: 'https://wallpapercave.com/wp/wp4056410.jpg',
+        title: 'A Netflix Orignal Serise',
+        sname: 'Dark-2',
+        link: 'https://www.netflix.com/in/title/80990668?source=35'
+    },
+    {
+        imgsrc: 'https://wallpapercave.com/wp/wp4056410.jpg',
+        title: 'A Netflix Orignal Serise',
+        sname: 'Dark-3',
+        link: 'https://www.netflix.com/in/title/80990668?source=35'
+    }
+]
+
+export default Sdata;
