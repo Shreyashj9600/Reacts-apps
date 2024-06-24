52f8231ee07bb73deb619a0f1053835df69602a0
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-06-24 17:03:01 +0530

    complet netflix project

---
+++ react 16/src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Cards from "./Cards";
+import './index.css';
+import Sdata from "./Sdata";
+
+
+ReactDom.render(
+  <>
+    <h1 className="heading_style">List of top 5 netflix Serise in 2024</h1>
+
+
+    {Sdata.map((val, index) => {
+      console.log(index)
+      return (
+        <Cards
+          key={val.id}
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      )
+    })}
+
+    {/* using function keyword */}
+    {/* {Sdata.map(function ncard(val) {
+      return (
+        <Cards
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      )
+    })} */}
+
+  </>
+  ,
+  document.getElementById('root')
+)
